388059ccdbf1146906fba56a1d2c8719f8f789b8
bismansahni <bismansahni@outlook.com>
2024-12-02 11:06:18 -0700

    feat:added credit check just after start interview

--- app/controllers/credit_controller.py
+++ app/controllers/credit_controller.py
@@ -14,7 +14,8 @@ def credit_decrement():
         user_doc = user_ref.get()
 
         # Get current credits and decrement
-        current_credits = user_doc.to_dict()['credits']  # Assume 'credits' key is always present
+        current_credits = user_doc.to_dict()['credits']
+
         if current_credits > 0:
             new_credits = current_credits - 1
             user_ref.update({'credits': new_credits})
@@ -23,6 +24,32 @@ def credit_decrement():
         else:
             print("No credits available to deduct.")
             return "Error: No credits available to deduct."
+    except Exception as e:
+        print(f"An error occurred: {str(e)}")
+        return f"Error: {str(e)}"
+
+
+
+
+
+def check_credit():
+    try:
+        data=request.get_json()
+        uid = data.get('uid')
+        if not uid:
+            return jsonify({"error": "Missing uid"}), 400
+        # Fetch the user document
+        user_ref = db.collection('users').document(uid)
+        user_doc = user_ref.get()
+
+        # Get current credits and decrement
+        current_credits = user_doc.to_dict()['credits']  # Assume 'credits' key is always present
+
+        if current_credits > 0:
+            return jsonify({"message": "Credits available", "current_credits": current_credits}), 200
+        else:
+            print("No credits available to deduct.")
+            return jsonify({"message": "No credits available", "current_credits": current_credits}), 403
     except Exception as e:
         print(f"An error occurred: {str(e)}")
         return f"Error: {str(e)}"
--- app/routes/credit_routes.py
+++ app/routes/credit_routes.py
@@ -1,8 +1,13 @@
 from flask import Blueprint, request, jsonify
-from app.controllers.credit_controller import  credit_decrement
+from app.controllers.credit_controller import credit_decrement, check_credit
 
 credit_bp = Blueprint('credit', __name__)
 
 @credit_bp.route('/credit-decrement', methods=['POST'])
 def credits():
     return credit_decrement()
+
+
+@credit_bp.route('/check-credit', methods=['POST'])
+def credit_check():
+    return check_credit()
